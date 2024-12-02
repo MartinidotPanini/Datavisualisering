94aa890de544bcf63cf5cd553d69ec059c409979
imposter9901 <imposter9901@gmail.com>
2024-12-02 22:38:16 +0100

    Update loadJSON calls in sketch.js to include holiday API and add printDate function

--- sketch.js
+++ sketch.js
@@ -2,9 +2,18 @@ let time = "http://worldtimeapi.org/api/ip"
 let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2023"
 
 function setup() {
-  loadJSON(time, printData);
+  loadJSON(time, printDate);
+  loadJSON(urlAPI, printData);
+}
+function printDate(data) {
+  let date = data.datetime;
+  console.log(date);
 }
 
-function printData(data) {
-  console.log(data);
+function printData(holidaysData) {
+  console.log(holidaysData.holidays[0]);
+
+  let nextHoliday = holidaysData.holidays[0];
+  let date = nextHoliday.date;
+
 }
