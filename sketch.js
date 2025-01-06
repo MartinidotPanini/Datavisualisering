509b5b6f8f0f72a66fc4bf76b2c7d585891fc22a
MartinidotPanini <miali2005.dk@gmail.com>
2025-01-06 14:16:13 +0100

    v2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let time = "http://worldtimeapi.org/api/ip"
+let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2023"
+
 function setup() {
-  createCanvas(400, 400);
+  loadJSON(time, printDate);
+  loadJSON(urlAPI, printData);
+}
+function printDate(data) {
+  let date = data.datetime;
+  console.log(date);
 }
 
-function draw() {
-  background(100);
+function printData(holidaysData) {
+  console.log(holidaysData.holidays[0]);
+
+  let nextHoliday = holidaysData.holidays[0];
+  let date = nextHoliday.date;
+
 }
