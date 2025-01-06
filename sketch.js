f25973ebb249afb0b9803967feae4727fac9aed4
MartinidotPanini <miali2005.dk@gmail.com>
2025-01-06 15:12:14 +0100

    6/1

--- sketch.js
+++ sketch.js
@@ -1,19 +1,41 @@
-let time = "http://worldtimeapi.org/api/ip"
-let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2023"
+let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2024";
+
+// Set `adjustedDate` to your desired date or `null` to use today's actual date.
+let adjustedDate = new Date("September 27, 2024 08:00:00"); // Replace with desired date or set to null
 
 function setup() {
-  loadJSON(time, printDate);
   loadJSON(urlAPI, printData);
-}
-function printDate(data) {
-  let date = data.datetime;
-  console.log(date);
 }
 
 function printData(holidaysData) {
-  console.log(holidaysData.holidays[0]);
+  let holidays = holidaysData.holidays;
+  
+  // Use adjusted date if provided, otherwise use the actual current date
+  let today = adjustedDate || new Date();
+  console.log("Today's Date:", today);
 
-  let nextHoliday = holidaysData.holidays[0];
-  let date = nextHoliday.date;
+  let nextHoliday = null;
 
+  for (let i = 0; i < holidays.length; i++) {
+    let holidayDate = new Date(holidays[i].date);
+    if (holidayDate > today) { 
+      nextHoliday = holidays[i];
+      break;
+    }
+  }
+
+  if (nextHoliday) {
+    console.log("Next Holiday:", nextHoliday.name, "on", nextHoliday.date);
+
+    // Convert the next holiday date to a timestamp
+    let holidayDate = new Date(nextHoliday.date);
+    let holidayTimestamp = holidayDate.getTime();
+    console.log("Holiday Timestamp:", holidayTimestamp);
+
+    // Calculate the number of days until the next holiday
+    let daysUntilHoliday = Math.floor((holidayTimestamp - today.getTime()) / (1000 * 60 * 60 * 24));
+    console.log("Days Until Next Holiday:", daysUntilHoliday);
+  } else {
+    console.log("No Future Holidays Found.");
+  }
 }
