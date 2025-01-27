e970a66800298f3731ba9d884574d039ef86b764
imposter9901 <imposter9901@gmail.com>
2025-01-27 15:16:10 +0100

    Added sketcg.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 let time = "http://worldtimeapi.org/api/ip"
-let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2023"
+let urlAPI = "https://holidayapi.com/v1/holidays?pretty&key=f56c3874-4a1c-401e-aeb9-0baee40626a6&country=DK&year=2024"
 
 function setup() {
   loadJSON(time, printDate);
   loadJSON(urlAPI, printData);
 }
-function printDate(data) {
-  let date = data.datetime;
+
+function printDate(date) {
+  let date = date.datetime;
   console.log(date);
 }
 
 function printData(holidaysData) {
-  console.log(holidaysData.holidays[0]);
+  
+  let holidaysData = nextHoliday.date;
 
-  let nextHoliday = holidaysData.holidays[0];
-  let date = nextHoliday.date;
+  console.log(holidaysData);
+
 
 }
